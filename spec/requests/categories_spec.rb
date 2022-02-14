1e4ed8fb475b1e5be46edb307c3463a07c682962
Keith Pineda <keith.m.pineda@gmail.com>
2022-02-14 16:51:01 +0800

    successful test on user, category and task

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -5,13 +5,13 @@ require 'devise/jwt/test_helpers'
 RSpec.describe "Categories", type: :request do
   describe "GET /categories" do
     it 'returns all categories' do
-      user = User.create(email: 'testing@gmail.com', password: 'password')
+      user = User.create(email: 'email@email.com', password: 'password')
       # user = FactoryBot.create(:user, email: "testing@gmail.com", password: "password")
       headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
       
       auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
-      c1 = FactoryBot.create(:category, name: 'testing', description: 'this is a description', user:user)
-      c2 = FactoryBot.create(:category, name: 'testing2', description: 'this is another description', user:user)
+      c1 = FactoryBot.create(:category, name: 'name', description: 'this is a description', user:user)
+      c2 = FactoryBot.create(:category, name: 'name', description: 'this is another description', user:user)
 
       get '/api/v1/categories', headers: auth_headers
 
@@ -20,11 +20,11 @@ RSpec.describe "Categories", type: :request do
     end
 
     it 'returns a category' do
-      user = FactoryBot.create(:user, email: 'test@gmail.com', password: 'testing')
+      user = FactoryBot.create(:user, email: 'email@email.com', password: 'password')
       headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
    
       auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
-      cat = FactoryBot.create(:category, name: 'testing', description: 'this is a description', user:user)
+      cat = FactoryBot.create(:category, name: 'name', description: 'this is a description', user:user)
 
       get "/api/v1/categories/#{cat.id}", headers: auth_headers
 
@@ -35,12 +35,12 @@ RSpec.describe "Categories", type: :request do
 
   describe "POST /categories" do
     it 'creates a new category' do
-      user = FactoryBot.create(:user, email: 'test@gmail.com', password: 'testing')
+      user = FactoryBot.create(:user, email: 'email@email.com', password: 'password')
       headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
    
       auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
       expect {
-        post '/api/v1/categories', headers: auth_headers, params: {category: {name: 'this is a name', description: 'this is a description', user_id: user.id}}.to_json
+        post '/api/v1/categories', headers: auth_headers, params: {category: {name: 'name', description: 'this is a description', user_id: user.id}}.to_json
         
       }.to change { Category.count }.from(0).to(1)
     
@@ -64,23 +64,23 @@ RSpec.describe "Categories", type: :request do
     # end
 
     it 'should update the category data' do
-      user = FactoryBot.create(:user, email: 'test@gmail.com', password: 'testing')
+      user = FactoryBot.create(:user, email: 'email@email.com', password: 'password')
       headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
    
       auth_headers = Devise::JWT::TestHelpers.auth_headers(headers, user)
-      cat = FactoryBot.create(:category, name: 'testing', description: 'description', user:user)
-      put "/api/v1/categories/#{cat.id}", params: {category: {name: 'kitkat', description: 'i love kitkat'}}.to_json, headers: auth_headers
+      cat = FactoryBot.create(:category, name: 'name', description: 'description', user:user)
+      put "/api/v1/categories/#{cat.id}", params: {category: {name: 'name', description: 'description'}}.to_json, headers: auth_headers
 
       expect(response).to have_http_status(200)
       json_body = JSON.parse(response.body)
-      expect(json_body['name']).to eq('kitkat')
-      expect(json_body['description']).to eq('i love kitkat')
+      expect(json_body['name']).to eq('name')
+      expect(json_body['description']).to eq('description')
     end
   end
 
   describe 'DELETE /categories/:id' do
-    let!(:user) { FactoryBot.create(:user, email: 'test@gmail.com', password:'testing')}
-    let!(:cat) { FactoryBot.create(:category, name: 'testing', description: 'this is a description', user: user) }
+    let!(:user) { FactoryBot.create(:user, email: 'email@email.com', password:'password')}
+    let!(:cat) { FactoryBot.create(:category, name: 'name', description: 'this is a description', user: user) }
     it 'deletes a category' do
       headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
    
