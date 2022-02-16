fa57560475bd39a9796722f73afdd51a2dcf1e6e
Keith Pineda <keith.m.pineda@gmail.com>
2022-02-16 15:07:42 +0800

    add index on show only task on routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
   namespace :v1 do
   resources :categories, only: [:index, :show, :create, :update, :destroy]
-  resources :tasks, only: [:show, :create, :update, :destroy]
+  resources :tasks, only: [:show, :create, :update, :destroy, :index]
   resources :members, only: [:index]
     end
   end
